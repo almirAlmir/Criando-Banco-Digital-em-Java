f6172cb792bfcef2ed40b8e434fa360370f76cb1
almirAlmir <aasc@cin.ufpe.br>
2022-11-08 21:07:02 -0300

    atualizando Main

--- bedroomBank/Mainbanco.java
+++ bedroomBank/Mainbanco.java
@@ -4,14 +4,14 @@ public class Mainbanco {
 
 	public static void main(String[] args) {
 		Banco banco1 = new Banco();
-		Cliente cliente1 = new Cliente("Almir Alves", "11312014423", "Jacirley Alves da Silva");
+		Cliente cliente1 = new Cliente("Almir Alves", "33322244423", "Jacy Alves da Silva");
 		Conta c1 = new Corrente(425, "4002-19978-21", cliente1);
 		
 		System.out.println(banco1.addConta(c1));
 		c1.depositar(45.50);
 		
 		System.out.println(banco1.getContas());
-		Cliente cliente2 = new Cliente("Vitoria Raisa", "43405894468", "Maria");
+		Cliente cliente2 = new Cliente("Vitoria Raisa", "44488844468", "Maria");
 		Conta c2 = new Poupanca(194, "977865-h32", cliente2);
 		
 		
